238a259d91f84721198f334007fd92e815fdda55
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-17 15:57:09 -0300

    a

--- nix-config/modules/eww.nix
+++ nix-config/modules/eww.nix
@@ -1,5 +1,5 @@
 {inputs, pkgs, ...}: 
 {
   programs.eww.enable = true;
-  programs.eww.configDir = "../dotfiles/eww"
+  programs.eww.configDir = "../dotfiles/eww";
 }
--- nix-config/modules/nvim.nix
+++ nix-config/modules/nvim.nix
@@ -1,5 +1,5 @@
 {inputs, pkgs, ...}: 
 {
   programs.neovim.enable = true;
-  xdg.configFile.nvim.source = ../dotfiles/nvim
+  xdg.configFile.nvim.source = "../dotfiles/nvim";
 }
