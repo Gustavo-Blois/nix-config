8e7a365af44ea4124fb5b1a853ea46d9120b1e7b
Gustavo-Blois <104032155+Gustavo-Blois@users.noreply.github.com>
2024-04-18 00:09:43 -0300

    a

---
+++ Lua/hypr/start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+#wpp=$(($RANDOM%1))
+swww init &
+swww img ~/Wallpapers/0
+nm-applet --indicator &
+eww &
+waybar &
+dunst
--- Sol/hypr/start.sh
+++ Sol/hypr/start.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 swww init &
-sleep 1
-swww img ~/Wallpapers/1
+swww img ~/Wallpapers/1 &
 nm-applet --indicator &
 waybar &
 dunst
