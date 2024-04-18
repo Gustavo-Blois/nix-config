9b16a863154be2b3c4a985f55c395a7a3b70153f
Gustavo-Blois <104032155+Gustavo-Blois@users.noreply.github.com>
2024-04-17 23:56:23 -0300

    a

--- nix-config/home-manager-modules/hyprland.nix
+++ nix-config/home-manager-modules/hyprland.nix
@@ -1,17 +1,6 @@
 {inputs, pkgs, ...}: 
-  let
-    startupScript = pkgs.pkgs.writeShellScriptBin "start" ''
-    swww init &
-    sleep 1
-    swww img ~/Wallpapers/1
-    nm-applet --indicator &
-    waybar &
-    dunst
-    '';
-  in{
-  #xdg.configFile."hypr".source = ../dotfiles/hypr;
-  wayland.windowManager.hyprland = {
-    extraConfig = ''exec-once = ${startupScript}/bin/start '';
-  };
-
+{
+  xdg.configFile."hypr".source = ../Sol/hypr;
 }
+
+
