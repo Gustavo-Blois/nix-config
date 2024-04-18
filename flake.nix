0b4990ed4a03200f5c8d328ab774b4f61e5a3ff2
Gustavo-Blois <104032155+Gustavo-Blois@users.noreply.github.com>
2024-04-18 02:56:46 -0300

    a

--- flake.nix
+++ flake.nix
@@ -34,11 +34,6 @@
         modules = [
           ./Lua/configuration.nix
           inputs.home-manager.nixosModules.home-manager
-          {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.mason = import ./home.nix;
-          }
         ];
       };
     
