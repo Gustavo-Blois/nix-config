816770294d758b75747f6d271704f65224367454
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-19 00:22:53 -0300

    a

--- home-manager-modules/misc.nix
+++ home-manager-modules/misc.nix
@@ -1,5 +1,5 @@
 {inputs, pkgs, ...}: 
 {
-    programs.git.userEmail = "gustavobloisrr@gmail.com"
-    programs.git.userName = "Gustavo-Blois"
+    programs.git.userEmail = "gustavobloisrr@gmail.com";
+    programs.git.userName = "Gustavo-Blois";
 }
