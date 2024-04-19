1964261d7f446500c68e6fca8b80e07034d7bf96
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-19 01:55:09 -0300

    a

--- dotfiles/nvim/lua/plugins/example.lua
+++ dotfiles/nvim/lua/plugins/example.lua
@@ -75,11 +75,6 @@ return {
       },
     },
   },
-  {
-    'mrcjkb/haskell-tools.nvim',
-    version = '^3', -- Recommended
-    lazy = false, -- This plugin is already lazy
-  }
   -- add tsserver and setup with typescript.nvim instead of lspconfig
   {
     "neovim/nvim-lspconfig",
--- home-manager-modules/nvim.nix
+++ home-manager-modules/nvim.nix
@@ -2,4 +2,7 @@
 {
   programs.neovim.enable = true;
   xdg.configFile."nvim".source = ../dotfiles/nvim;
+  programs.neovim.plugins = [
+  pkgs.vimPlugins.haskell-tools.nvim
+  ];
 }
