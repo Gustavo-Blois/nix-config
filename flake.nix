2266f14fb07571069eee8be201b309a084434268
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-23 11:08:38 -0300

    a

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A simple NixOS flake";
-
+  #
   inputs = {
     # NixOS official package source, using the nixos-23.11 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
