5f80f643e4297ed92ac819f4e9bdc4b034039af3
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-25 16:28:17 -0300

    a

---
+++ nixos-modules/zsh.nix
@@ -0,0 +1,15 @@
+{ inputs, outputs, config, pkgs, ... }:
+{
+users.defaultUserShell=pkgs.zsh; 
+programs = {
+   zsh = {
+      enable = true;
+      autosuggestions.enable = true;
+      zsh-autoenv.enable = true;
+      syntaxHighlighting.enable = true;
+      ohMyZsh = {
+         enable = true;
+      };
+   };
+};
+}
