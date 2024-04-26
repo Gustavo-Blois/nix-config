2299e99f46a26686683fb0e9eef633cacd75c389
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-26 13:09:28 -0300

    a

--- home-manager-modules/hyprland-lua.nix
+++ home-manager-modules/hyprland-lua.nix
@@ -2,7 +2,5 @@
 {
   xdg.configFile."hypr".source = ../Lua/hypr;
   xdg.configFile."waybar".source = ../Lua/waybar;
-  xdg.configFile."oh-my-zsh".source = ../Lua/zsh_custom;
-  programs.zsh.oh-my-zsh.custom = "~/.config/oh-my-zsh";
-  programs.zsh.oh-my-zsh.theme = "obraun-custom";
+  programs.zsh.oh-my-zsh.theme = "itchy";
 }
