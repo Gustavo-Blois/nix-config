9d8b5b73922d0f50f353a0b3afd0beeeb1fd0863
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-04-30 23:12:26 -0300

    a

--- Sol/configuration.nix
+++ Sol/configuration.nix
@@ -18,7 +18,7 @@
 
 
 
-  networking.hostName = "Sol"; # Define your hostname.
+  networking.hostName = "gr1mb0rk"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
