94201a689c79be02263468f9817bf6cd73d97ef4
Gustavo-Blois <gustavobloisrr@gmail.com>
2024-05-07 17:40:19 -0300

    a

--- Sol/configuration.nix
+++ Sol/configuration.nix
@@ -17,14 +17,22 @@
     ];
 
 
-
+  
   networking.hostName = "gr1mb0rk"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+ 
+  xdg.portal = {
+    enable = true;
+    extraPortals = with pkgs; [ xdg-desktop-portal-gtk ];
+  };
 
+  environment.systemPackages = with pkgs; [
+    xdg-desktop-portal-gtk
+  ];
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.mason = {
